2508d20a2b4bc6d011d0dc9290d434c8e590b77a
Viktoriia Holovatiuk <holovatiuk.victoria@gmail.com>
2022-05-24 01:32:11 -0400

    Created one scenario and developed step defs for it

--- src/test/java/definitions/LoginSteps.java
+++ src/test/java/definitions/LoginSteps.java
@@ -6,6 +6,7 @@ package definitions;
 
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 
@@ -51,4 +52,30 @@ public class LoginSteps {
     public void iWaitForSeconds(int sec) throws InterruptedException {
         Thread.sleep(sec * 1000);
     }
+
+    @When("I type first name {string}")
+    public void iTypeFirstName(String firstName) {
+        getDriver().findElement(By.xpath("//input[@formcontrolname=\"firstName\"]")).sendKeys(firstName);
+    }
+
+    @And("I type last name {string}")
+    public void iTypeLastName(String lastName) {
+        getDriver().findElement(By.xpath("//input[@formcontrolname=\"lastName\"]")).sendKeys(lastName);
+    }
+
+    @And("I type email {string}")
+    public void iTypeEmail(String email) {
+        getDriver().findElement(By.xpath("//input[@formcontrolname=\"email\"]")).sendKeys(email);
+    }
+
+    @And("I type group code {string}")
+    public void iTypeGroupCode(String groupCode) {
+        getDriver().findElement(By.xpath("//input[@formcontrolname=\"group\"]")).sendKeys(groupCode);
+    }
+
+    @And("I type confirm password {string}")
+    public void iTypeConfirmPassword(String confirmPassword) {
+        getDriver().findElement(By.xpath("//input[@formcontrolname=\"confirmPassword\"]")).sendKeys(confirmPassword);
+    }
+
 }
