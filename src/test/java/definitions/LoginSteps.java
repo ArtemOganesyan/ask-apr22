8cd611e435352fff7393f5c5c5732a8b1a2bed65
Eugene1610 <ievgen.truniakov@gmail.com>
2022-05-24 03:05:18 -0700

    Feature Change User Name

--- src/test/java/definitions/LoginSteps.java
+++ src/test/java/definitions/LoginSteps.java
@@ -7,6 +7,9 @@ package definitions;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -39,8 +42,7 @@ public class LoginSteps {
         System.out.println(r);
         if (role.equals("STUDENT")) {
             assertThat(r).isEqualTo("STUDENT");
-        }
-        else if (role.equals("TEACHER")) {
+        } else if (role.equals("TEACHER")) {
             assertThat(r).isEqualTo("TEACHER");
         } else {
             Assert.fail("User role not found");
@@ -49,17 +51,53 @@ public class LoginSteps {
 
     @And("I wait for {int} seconds")
     public void iWaitForSeconds(int sec) throws InterruptedException {
-        Thread.sleep(sec * 1000);
+        Thread.sleep(sec * 2000);
     }
 
-    @Then("I click {string} button")
-    public void iClickButton(String arg0) {getDriver().findElement(By.xpath("//h5[contains(@text(),'My Grades')]")).click();
+//    @Then("I click {string} button")
+//    public void iClickButton(String arg0) {getDriver().findElement(By.xpath("//h5[contains(@text(),'My Grades')]")).click();
+//    }
+
+    @Then("I click Settings button")
+    public void iClickSettingsButton() {
+        getDriver().findElement(By.xpath("//h5[contains(text(),'Settings')]")).click();
+
     }
-}
+
+    @Then("I click on Change Your Name button")
+    public void iClickOnChangeYourNameButton() {
+
+        getDriver().findElement(By.xpath("//span[contains(text(),'Change Your Name')]")).click();
+
+    }
+
+    @And("I type New Name {string}")
+    public void iTypeNewName(String newName) {
+
+        WebElement nameField = getDriver().findElement(By.xpath("//input[@placeholder='New name']"));
+
+        Actions a = new Actions(getDriver());
+
+        a.moveToElement(nameField).sendKeys(Keys.BACK_SPACE).sendKeys(newName).build().perform();
+
+    }
+
+    @Then("I click on Change button")
+    public void iClickOnChangeButton() {
+        getDriver().findElement(By.xpath("//button[@type='button'][2]")).click();
+    }
 
 
-    @And("I wait for {int} seconds")
-    public void iWaitForSeconds(int sec) throws InterruptedException {
-        Thread.sleep(sec * 3000);
+    @And("I verify a New Name is changed to {string}")
+    public void iVerifyANewNameIsChangedTo(String nameVer) {
+        String nam = getDriver().findElement(By.xpath("//tr//td[contains(text(),'David')]")).getText();
+        System.out.println(nam);
+        if (nameVer.equals("David")) {
+            assertThat(nameVer).isEqualTo("David");
+        } else {
+            Assert.fail("User name is not changed");
+
+        }
+
     }
 }
--- src/test/resources/features/ievgen.feature
+++ src/test/resources/features/ievgen.feature
@@ -1,1 +1,19 @@
-@initial.test
+@smokeTeacher
+  Feature: Change User Name
+    Scenario: Login as a teacher
+      Given I open url "http://ask-stage.portnov.com"
+      Then I type login "testerhanoi1610+2@gmail.com"
+      And I type password "22222"
+      Then I click submit button
+      And I wait for 2 seconds
+      And I verify current user role set to "TEACHER"
+      Then I click Settings button
+      And I wait for 2 seconds
+      Then I click on Change Your Name button
+      And I type New Name "David"
+      And I wait for 2 seconds
+      Then I click on Change button
+      And I verify a New Name is changed to "David"
+
+
+
