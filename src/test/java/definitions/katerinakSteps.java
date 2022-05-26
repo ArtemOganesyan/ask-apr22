386eaca60aef9b1379068d15e6c9b751f21f274d
Katerina Kholoshenko <katerina.kholoshenko@gmail.com>
2022-05-26 16:03:08 -0700

    Updated

--- src/test/java/definitions/katerinakSteps.java
+++ src/test/java/definitions/katerinakSteps.java
@@ -3,24 +3,80 @@ package definitions;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static support.TestContext.getDriver;
 
 public class katerinakSteps {
-    @Given("I type url {string}")
-    public void iTypeUrl(String url) {
+//    @Given("I type url {string}")
+//    public void iTypeUrl(String url) {
+//        getDriver().get(url);
+//    }
+
+//    @Then("I type email as {string}")
+//    public void iTypeEmailAs(String email) {
+//        getDriver().findElement(By.xpath("//input[@formcontrolname=\"email\"]")).sendKeys(email);
+//    }
+//
+//    @And("I type password as {string}")
+//    public void iTypePasswordAs(String password) {
+//        getDriver().findElement(By.xpath("//input[@formcontrolname=\"email\"]")).sendKeys(password);
+//    }
+
+    @Given("I KK type url {string}")
+    public void iKKTypeUrl(String url) {
         getDriver().get(url);
     }
 
-    @Then("I type email as {string}")
-    public void iTypeEmailAs(String email) {
+    @Then("I KK type email as {string}")
+    public void iKKTypeEmailAs(String email) {
         getDriver().findElement(By.xpath("//input[@formcontrolname=\"email\"]")).sendKeys(email);
     }
 
-    @And("I type password as {string}")
-    public void iTypePasswordAs(String password) {
-        getDriver().findElement(By.xpath("//input[@formcontrolname=\"password\"]")).sendKeys(password);
+    @And("I KK type password as {string}")
+    public void iKKTypePasswordAs(String password) {
+        getDriver().findElement(By.xpath("//input[@formcontrolname=\"email\"]")).sendKeys(password);
     }
 
+    @Then("I KK click on Settings")
+    public void iKKClickOnSettings() {
+        getDriver().findElement(By.xpath("//h5[contains(text(),\"Settings\")]"));
+    }
+
+    @Then("I KK click on button Change Your Password")
+    public void iKKClickOnButtonChangeYourPassword() {
+        getDriver().findElement(By.xpath("//span[contains(text(),\"Change Your Password\")]"));
+    }
+
+    @Then("I KK type old password {string}")
+    public void iKKTypeOldPassword(String oldPassword) {
+        getDriver().findElement(By.xpath("//input[@id=\"mat-input-2\"]")).sendKeys(oldPassword);
+    }
+
+    @And("I KK type new password {string}")
+    public void iKKTypeNewPassword(String newPassword) {
+        getDriver().findElement(By.xpath("//input[@id=\"mat-input-3\"]")).sendKeys(newPassword);
+    }
+
+    @And("I KK confirm new password {string}")
+    public void iKKConfirmNewPassword(String newPassword) {
+        getDriver().findElement(By.xpath("//input[@id=\"mat-input-4\"]")).sendKeys(newPassword);
+    }
+
+    @Then("I KK click button Change")
+    public void iKKClickButtonChange() {
+        getDriver().findElement(By.xpath("//span[contains(text(), \"Change\")]")).click();
+    }
+
+    @And("I KK verify that error message is displayed {string}")
+    public void iKKVerifyThatErrorMessageIsDisplayed(String errorMessage) {
+        String message = getDriver().findElement(By.xpath("//mat-error[@id=\"mat-error-9\"]")).getText();
+        if (errorMessage.equals("This field is required")) {
+            assertThat(message).isEqualTo("This field is required");
+        } else {
+            Assert.fail("Password Changed");
+        }
+    }
 }
--- src/test/resources/features/katerinak.feature
+++ src/test/resources/features/katerinak.feature
@@ -3,15 +3,26 @@
 
     @scenario1
     Scenario: Password allows alphanumeric and special characters
-      Given I type url "http://ask-stage.portnov.com"
-      Then I type email as "mikeb512@teeenye.com"
-      And I type password as "12345"
-      Then I click on Settings
-      Then I click on button Change Your Password
-      Then I type old password "12345"
-      And I type new password "12345!@#$%^&*"
-      And I confirm new password "12345!@#$%^&*"
-      Then I click button Change
+      Given I KK type url "http://ask-stage.portnov.com"
+      Then I KK type email as "mikeb512@teeenye.com"
+      And I KK type password as "12345"
+      Then I KK click on Settings
+      Then I KK click on button Change Your Password
+      Then I KK type old password "12345"
+      And I KK type new password "12345!@#$%^&*"
+      And I KK confirm new password "12345!@#$%^&*"
+      Then I KK click button Change
+
+      @scenario2
+      Scenario: Password field is required and cannot be empty
+        Given I KK type url "http://ask-stage.portnov.com"
+        Then I KK type email as "mikeb512@teeenye.com"
+        And I KK type password as "12345"
+        Then I KK click on Settings 
+        Then I KK click on button Change Your Password
+        Then I KK type new password "123456"
+        Then I KK confirm new password "123456"
+        And I KK verify that error message is displayed "This field is required"
 
 
 
